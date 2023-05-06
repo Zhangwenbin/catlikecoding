484fd94f96cf61dcb48a3a1c66181e72a4ae0dc5
zhangwenbin <zhangwenbin.dayu@bytedance.com>
2023-05-06 10:30:17 +0800

    圆环

--- UnityProject/Assets/Basics/VisualizingMath/FunctionLibrary3D.cs
+++ UnityProject/Assets/Basics/VisualizingMath/FunctionLibrary3D.cs
@@ -12,10 +12,13 @@ public class FunctionLibrary3D
         Sphere,
         Spherewithverticalbands,
         Spherewithhorizantalbands,
+        Rotatingtwistedsphere,
+        Torus,
+        Twistingtorus
     }
     public delegate Vector3 Function (float u, float v, float t);
 
-    private static Function[] _functions = {Wave,MultiWave,Ripple ,Sphere,Spherewithverticalbands,Spherewithhorizantalbands};
+    private static Function[] _functions = {Wave,MultiWave,Ripple ,Sphere,Spherewithverticalbands,Spherewithhorizantalbands,Rotatingtwistedsphere,Torus,Twistingtorus};
 
     public static Function GetFunction(FunctionName name)
     {
@@ -76,6 +79,38 @@ public class FunctionLibrary3D
         Vector3 p;
         p.x = s * Sin(PI * u);
         p.y = r * Sin(0.5f * PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+    
+    public static Vector3 Rotatingtwistedsphere (float u, float v, float t) {
+        float r = 0.9f + 0.1f * Sin( PI * (6*u+4*v+t));
+        float s = r * Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(0.5f * PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+    
+    public static Vector3 Torus (float u, float v, float t) {
+        float r1 = 0.75f;
+        float r2 = 0.25f;
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+    
+    public static Vector3 Twistingtorus (float u, float v, float t) {
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
         p.z = s * Cos(PI * u);
         return p;
     }
