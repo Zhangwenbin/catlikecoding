bf14aaad32253c626d67fe971fde3cdbf097cf59
zhangwenbin <zhangwenbin.dayu@bytedance.com>
2023-05-17 15:39:15 +0800

    hash Space

---
+++ UnityProject/Assets/PseudorandomNoise/HashingSpace/HashSpaceVisualization.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+using static Unity.Mathematics.math;
+public class HashSpaceVisualization : MonoBehaviour
+{
+    [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+    struct HashJob : IJobFor {
+
+        [ReadOnly]
+        public NativeArray<float3> positions;
+        
+        [WriteOnly]
+        public NativeArray<uint> hashes;
+        
+        // public int resolution;
+        //
+        // public float invResolution;
+        
+        public SmallXXHash hash;
+        
+        public float3x4 domainTRS;
+        public void Execute(int i) {
+            //hashes[i] = (uint)i;
+            
+           // hashes[i] = (uint)(frac(i * 0.381f) * 256f);
+           
+         //  float v = floor( i/resolution + 0.00001f);
+         
+            // float v = floor(invResolution * i + 0.00001f);
+            // float u = i - resolution * v;
+            // hashes[i] = (uint)(frac(u * v * 0.381f) * 255f);
+            
+            // int v = (int)floor(invResolution * i + 0.00001f);
+            // int u = i - resolution * v;
+            
+            // float vf = floor(invResolution * i + 0.00001f);
+            // float uf = invResolution * (i - resolution * vf + 0.5f) - 0.5f;
+            // vf = invResolution * (vf + 0.5f) - 0.5f;
+
+            float3 p = mul(domainTRS, float4(positions[i], 1f));
+
+            int u = (int)floor(p.x);
+            int v = (int)floor(p.y);
+            int w = (int)floor(p.z);
+
+            hashes[i] = hash.Eat(u).Eat(v).Eat(w);
+        }
+    }
+    
+    static int
+        hashesId = Shader.PropertyToID("_Hashes"),
+        positionsId = Shader.PropertyToID("_Positions"),
+        configId = Shader.PropertyToID("_Config");
+
+    [SerializeField]
+    Mesh instanceMesh;
+
+    [SerializeField]
+    Material material;
+
+    [SerializeField, Range(1, 512)]
+    int resolution = 16;
+    
+    [SerializeField]
+    int seed;
+    
+    [SerializeField, Range(-2f, 2f)]
+    float verticalOffset = 1f;
+    
+    [SerializeField]
+    SpaceTRS domain = new SpaceTRS {
+        scale = 8f
+    };
+
+    NativeArray<uint> hashes;
+
+    ComputeBuffer hashesBuffer, positionsBuffer;
+
+    MaterialPropertyBlock propertyBlock;
+    
+    NativeArray<float3> positions;
+    
+    void OnEnable () {
+        int length = resolution * resolution;
+        hashes = new NativeArray<uint>(length, Allocator.Persistent);
+        positions = new NativeArray<float3>(length, Allocator.Persistent);
+        hashesBuffer = new ComputeBuffer(length, 4);
+        positionsBuffer = new ComputeBuffer(length, 3 * 4);
+        
+        JobHandle handle = Shapes.Job.ScheduleParallel(positions, resolution, default);
+        new HashJob {
+            hashes = hashes,
+            positions = positions,
+            //resolution = resolution,
+            //invResolution = 1f/resolution,
+            hash=SmallXXHash.Seed(seed),
+            domainTRS = domain.Matrix
+        }.ScheduleParallel(hashes.Length, resolution, handle).Complete();
+
+        hashesBuffer.SetData(hashes);
+        positionsBuffer.SetData(positions);
+        
+        propertyBlock ??= new MaterialPropertyBlock();
+        propertyBlock.SetBuffer(hashesId, hashesBuffer);
+        propertyBlock.SetVector(configId, new Vector4(resolution, 1f / resolution, verticalOffset / resolution));
+        propertyBlock.SetBuffer(positionsId, positionsBuffer);
+    }
+    
+    void OnDisable () {
+        hashes.Dispose();
+        positions.Dispose();
+        hashesBuffer.Release();
+        positionsBuffer.Release();
+        hashesBuffer = null;
+        positionsBuffer = null;
+    }
+
+    void OnValidate () {
+        if (hashesBuffer != null && enabled) {
+            OnDisable();
+            OnEnable();
+        }
+    }
+    
+    void Update () {
+        Graphics.DrawMeshInstancedProcedural(
+            instanceMesh, 0, material, new Bounds(Vector3.zero, Vector3.one),
+            hashes.Length, propertyBlock
+        );
+    }
+}
