8f9b7da76d8d531ddff3a78a0c2d61c25250e97e
zhangwenbin <zhangwenbin.dayu@bytedance.com>
2023-05-18 11:21:47 +0800

    手动向量化

--- UnityProject/Assets/PseudorandomNoise/Hashing/SmallXXHash.cs
+++ UnityProject/Assets/PseudorandomNoise/Hashing/SmallXXHash.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public readonly struct SmallXXHash 
@@ -9,7 +10,7 @@ public readonly struct SmallXXHash
     const uint primeC = 0b11000010101100101010111000111101;
     const uint primeD = 0b00100111110101001110101100101111;
     const uint primeE = 0b00010110010101100110011110110001;
-    readonly uint accumulator;
+    public readonly uint accumulator;
     
     public SmallXXHash (uint accumulator) {
         this.accumulator = accumulator;
@@ -43,4 +44,46 @@ public readonly struct SmallXXHash
     public static SmallXXHash Seed (int seed) => (uint)seed + primeE;
     
        
+}
+
+public readonly struct SmallXXHash4 
+{
+    //const uint primeA = 0b10011110001101110111100110110001;
+    const uint primeB = 0b10000101111010111100101001110111;
+    const uint primeC = 0b11000010101100101010111000111101;
+    const uint primeD = 0b00100111110101001110101100101111;
+    const uint primeE = 0b00010110010101100110011110110001;
+    public readonly uint4 accumulator;
+    
+    public SmallXXHash4 (uint4 accumulator) {
+        this.accumulator = accumulator;
+    }
+    public uint4 ToUint () => accumulator;
+    
+    public static implicit operator uint4 (SmallXXHash4 hash) {
+        uint4 avalanche = hash.accumulator;
+        avalanche ^= avalanche >> 15;
+        avalanche *= primeB;
+        avalanche ^= avalanche >> 13;
+        avalanche *= primeC;
+        avalanche ^= avalanche >> 16;
+        return avalanche;
+    }
+    
+    public static implicit operator SmallXXHash4 (uint4 accumulator) =>
+        new SmallXXHash4(accumulator);
+    
+    public static uint4 ToUint (SmallXXHash4 hash) => hash.accumulator;
+    
+    public SmallXXHash4 Eat (int4 data) =>
+        RotateLeft(accumulator + (uint4)data * primeC, 17) * primeD;
+    
+    static uint4 RotateLeft (uint4 data, int steps) =>
+        (data << steps) | (data >> 32 - steps);
+    
+    public static SmallXXHash4 Seed (int4 seed) => (uint4)seed + primeE;
+    
+    public static implicit operator SmallXXHash4 (SmallXXHash hash) =>
+        new SmallXXHash4(hash.accumulator);
+
 }
